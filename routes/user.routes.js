3ce61760046e3f976c7b136e910b90c772ea8d47
Suaste Cherizola <jsuaste86@gmail.com>
2024-06-02 13:37:56 +0200

    concert/get populates artist with id, picture, name and group name

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User.model")
+const User = require("../models/User.model");
+const { trusted } = require("mongoose");
 
+//Should we add the middleware to protect the route? 
 router.get("/:userId", (req, res, next) => {
-  console.log("calling to get in route /user")
+  console.log("calling to get info of specific user")
   User.findById(req.params.userId)
   .then ( response => {
     if (response) {
-      console.log(response)
     const newResponse = {
+      _id: response._id,
       name: response.name,
       picture: response.picture,
       city: response.city,
       isArtist: response.isArtist,
+      followedArtists: response.followedArtists,
+      bookmarkedEvents: response.bookmarkedEvents,
       moreThanOne: response.moreThanOne,
       groupName: response.groupName,
       artistMembers: response.artistMembers,
@@ -23,10 +27,10 @@ router.get("/:userId", (req, res, next) => {
       artistAudio: response.artistAudio,
       artistGenre: response.artistGenre,
     }
-    
     res.json(newResponse);
+    } else {
+    res.json(null)
     }
-  
   })  
   .catch (error => {
     next(error)
@@ -34,14 +38,11 @@ router.get("/:userId", (req, res, next) => {
 });
 
 router.put("/", (req, res, next) => {
-  
-  console.log("Info recieved", req.body)
-  
   User.findByIdAndUpdate(req.body.userId, req.body.userInfo, {new:true})
   .then( (response) => {
-    console.log(response)
+    console.log("Respuesta del put:", response)
     if (response) {
-      res.status(200).json("Successful")
+      res.status(200).json(response)
       return
     }
     else {
@@ -53,5 +54,23 @@ router.put("/", (req, res, next) => {
     next(error)
   })
 })
+
+router.put("/bookmark", ( (req, res, next) => {
+  const {action, userId, property, newElement,} = req.body;
+  User.findByIdAndUpdate (userId, {[action]: {[property]: newElement}  }, { new: true} )
+    .then( response => {
+      if (response) {
+      res.json(response) }
+      else {
+        res.status(404).json({ error: "User not found" });
+      }
+    })
+    .catch(error => {
+      console.error("Unable to update", error);
+      next(error)
+    })
+
+  }))
+
 
 module.exports = router;
